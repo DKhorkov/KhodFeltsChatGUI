740780405fe503eb84f8d9e541aec28847bd3941
Dmitriy Khorkov <dskhorkov@avito.ru>
2026-04-30 13:11:45 +0300

    Linting

--- internal/errors/users.go
+++ internal/errors/users.go
@@ -5,5 +5,7 @@ import "errors"
 var (
 	ErrUserNotFound      = errors.New(`user not found`)
 	ErrUserAlreadyExists = errors.New(`user already exists`)
-	ErrUpdateUsername    = errors.New("pq: duplicate key value violates unique constraint \"users_username_key\"\n")
+	ErrUpdateUsername    = errors.New(
+		"pq: duplicate key value violates unique constraint \"users_username_key\"",
+	)
 )
--- internal/v2/handlers/profile/handler.go
+++ internal/v2/handlers/profile/handler.go
@@ -58,7 +58,8 @@ func (h *Handler) UpdateUser(in domains.UpdateUserDTO) (*domains.User, error) {
 	ctx := context.Background()
 
 	// Валидация username
-	if in.Username != nil && !validation.ValidateValueByRules(*in.Username, h.validationConfig.UsernameRegExps) {
+	if in.Username != nil &&
+		!validation.ValidateValueByRules(*in.Username, h.validationConfig.UsernameRegExps) {
 		return nil, h.errorsMapper.Map(customerrors.ErrInvalidUsername)
 	}
 
